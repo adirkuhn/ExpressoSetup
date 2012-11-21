c1fb5c2178c852e84be3d580064e1b2901a9f639
Gustavo <gupraga@gmail.com>
2012-11-21 15:22:18 -0200

    traduções

--- web/config.php
+++ web/config.php
@@ -4,13 +4,13 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+/*if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
-}
+}*/
 
 require_once dirname(__FILE__).'/../app/SymfonyRequirements.php';
 
@@ -25,27 +25,27 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
     <head>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="bundles/sensiodistribution/webconfigurator/css/install.css" media="all" />
-        <title>Symfony Configuration</title>
+        <title>Configurações do Expresso</title>
     </head>
     <body>
         <div id="symfony-wrapper">
             <div id="symfony-content">
                 <div class="symfony-blocks-install">
                     <div class="symfony-block-logo">
-                        <img src="bundles/sensiodistribution/webconfigurator/images/logo-big.gif" alt="Symfony logo" />
+                        <img src="bundles/sensiodistribution/webconfigurator/images/login_expresso.png" width="200" heidth="200" alt="Symfony logo" />
                     </div>
 
                     <div class="symfony-block-content">
-                        <h1>Welcome!</h1>
-                        <p>Welcome to your new Symfony project.</p>
+                        <h1>Olá!</h1>
+                        <p>Bem vindo ao Expresso Livre.</p>
                         <p>
-                            This script will guide you through the basic configuration of your project.
-                            You can also do the same by editing the ‘<strong>app/config/parameters.yml</strong>’ file directly.
+                            Este script irá guiá-lo através da configuração básica do seu projeto. 
+							Você também pode fazer o mesmo pela edição do arquivo ‘<strong>app/config/parameters.yml</strong>’ diretamente.
                         </p>
 
                         <?php if (count($majorProblems)): ?>
-                            <h2 class="ko">Major problems</h2>
-                            <p>Major problems have been detected and <strong>must</strong> be fixed before continuing:</p>
+                            <h2 class="ko">Principais Problemas</h2>
+                            <p>Alguns problemas foram detectados e <strong>precisam</strong> serem corrigidos antes de continuar:</p>
                             <ol>
                                 <?php foreach ($majorProblems as $problem): ?>
                                     <li><?php echo $problem->getHelpHtml() ?></li>
@@ -54,10 +54,11 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
                         <?php endif; ?>
 
                         <?php if (count($minorProblems)): ?>
-                            <h2>Recommendations</h2>
+                            <h2>Recomendações</h2>
                             <p>
-                                <?php if (count($majorProblems)): ?>Additionally, to<?php else: ?>To<?php endif; ?> enhance your Symfony experience,
-                                it’s recommended that you fix the following:
+                                <?php if (count($majorProblems)): ?>							
+								Além disso, para<?php else: ?>Para<?php endif; ?> melhorar a sua experiência com o Expresso,
+                                é recomendado que você corrigir o seguinte:
                             </p>
                             <ol>
                                 <?php foreach ($minorProblems as $problem): ?>
@@ -69,30 +70,29 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
                         <?php if ($symfonyRequirements->hasPhpIniConfigIssue()): ?>
                             <p id="phpini">*
                                 <?php if ($symfonyRequirements->getPhpIniConfigPath()): ?>
-                                    Changes to the <strong>php.ini</strong> file must be done in "<strong><?php echo $symfonyRequirements->getPhpIniConfigPath() ?></strong>".
+                                    Modificações no arquivo <strong>php.ini</strong> devem ser feitas em "<strong><?php echo $symfonyRequirements->getPhpIniConfigPath() ?></strong>".
                                 <?php else: ?>
-                                    To change settings, create a "<strong>php.ini</strong>".
+                                    Para modificar suas configurações, crie um "<strong>php.ini</strong>".
                                 <?php endif; ?>
                             </p>
                         <?php endif; ?>
 
                         <?php if (!count($majorProblems) && !count($minorProblems)): ?>
-                            <p class="ok">Your configuration looks good to run Symfony.</p>
+                            <p class="ok">Sua configuração parece boa para executar o Expresso Livre.</p>
                         <?php endif; ?>
 
                         <ul class="symfony-install-continue">
                             <?php if (!count($majorProblems)): ?>
-                                <li><a href="app_dev.php/_configurator/">Configure your Symfony Application online</a></li>
-                                <li><a href="app_dev.php/">Bypass configuration and go to the Welcome page</a></li>
+                                <li><a href="app_dev.php/_configurator/">Configure o Expresso Online</a></li>
+                                <li><a href="app_dev.php/">Ignora a configuração e ir para a página inicial</a></li>
                             <?php endif; ?>
                             <?php if (count($majorProblems) || count($minorProblems)): ?>
-                                <li><a href="config.php">Re-check configuration</a></li>
+                                <li><a href="config.php">Verificar configurações novamente</a></li>
                             <?php endif; ?>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="version">Symfony Standard Edition</div>
         </div>
     </body>
 </html>
